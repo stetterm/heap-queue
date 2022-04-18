cd46ac99c4b57c8251a187a5eef78f2dfd815587
Matt Stetter <matthew.stetter@gmail.com>
2022-04-17 20:13:15 -0400

    First stable build with basic features without complete testing.

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,21 @@
 pub mod sorted_queue {
     use std::collections::HashMap;
 
+    ///
+    /// Custom error type returned if
+    /// the value to search for does
+    /// not appear in the queue.
+    /// 
+    #[derive(Debug)]
+    pub struct NotInQueue;
+
+    impl std::fmt::Display for NotInQueue {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            write!(f, "Value not found in queue")
+        }
+    }
+
+    impl std::error::Error for NotInQueue {}
 
     ///
     /// A sorted priority queue that uses
@@ -22,9 +37,8 @@ pub mod sorted_queue {
     where T: PartialOrd + Ord + Copy,
           F: Eq + std::hash::Hash
     {
-        heap: Vec<(T, usize)>,
-        comp: Box<dyn Fn((T, usize), (T, usize)) -> bool>,
-        data: Vec<&'a F>,
+        heap: Vec<(T, &'a F)>,
+        comp: Box<dyn Fn((T, &'a F), (T, &'a F)) -> bool>,
         map: HashMap<&'a F, usize>,
     }
 
@@ -44,7 +58,6 @@ pub mod sorted_queue {
                         let (b, _) = y;
                         a > b
                     }),
-                    data: vec![],
                     map: HashMap::new(),
                 }
             } else {
@@ -55,7 +68,6 @@ pub mod sorted_queue {
                         let (b, _) = y;
                         a < b
                     }),
-                    data: vec![],
                     map: HashMap::new(),
                 }
             }
@@ -64,8 +76,6 @@ pub mod sorted_queue {
         fn swap(&mut self, i1: usize, i2: usize) {
             let (_, o1) = self.heap[i1];
             let (_, o2) = self.heap[i2];
-            let o1 = self.data[o1];
-            let o2 = self.data[o2];
             self.map.insert(o1, i2);
             self.map.insert(o2, i1);
             let temp = self.heap[i1];
@@ -120,45 +130,120 @@ pub mod sorted_queue {
             }
         }
 
+        ///
+        /// Puts the value and the associated
+        /// referrence in the pritority
+        /// queue.
+        /// 
         pub fn enq(&mut self, value: T, data: &'a F) {
-            let index = self.data.len();
-            self.data.push(&data);
             let new_index = self.heap.len();
-            self.heap.push((value, index));
-            self.map.insert(&data, new_index);
+            self.heap.push((value, data));
+            self.map.insert(data, new_index);
             self.sift_up(new_index);
         }
 
+        ///
+        /// Removes the value from the front
+        /// of the queue and returns the value
+        /// and associated referrence.
+        /// 
         pub fn deq(&mut self) -> Option<(T, &'a F)> {
             if self.heap.len() == 0 {
                 return None;
             }
-
             self.swap(0, self.heap.len()-1);
+            let (rem_val, rem_ref) = self.heap.remove(self.heap.len()-1);
             self.sift_down(0);
-            let (rem_val, rem_index) = self.heap.remove(self.heap.len()-1);
+            Some((rem_val, rem_ref))
+        }
 
-            let swap_val = self.map.get(self.data[self.data.len()-1])?;
-            let (swap_heap_val, _) = self.heap[*swap_val];
-            self.heap[*swap_val] = (swap_heap_val, rem_index);
+        ///
+        /// Tries to find the data object in the
+        /// queue and change its priority value,
+        /// fails if this value cannot be
+        /// found in the queue.
+        /// 
+        pub fn change_priority(&mut self, new_value: T, data: &'a F) 
+            -> Result<(), Box<dyn std::error::Error>> {
+            let index = match self.map.get(data) {
+                Some(i) => *i,
+                None => return Err(Box::new(NotInQueue)),
+            };
+            let (old_val, _) = self.heap[index];
+            self.heap[index] = (new_value, data);
+            if (self.comp)((old_val, data), (new_value, data)) {
+                self.sift_down(index);
+            } else {
+                self.sift_up(index);
+            }
+            Ok(())
+        }
 
-            let temp = self.data[rem_index];
-            let length = self.data.len();
-            self.data[rem_index] = self.data[length-1];
-            self.data[length-1] = temp;
-            
-            let ret_val = self.data.remove(self.data.len()-1);
-            Some((rem_val, ret_val))
+        ///
+        /// Tries to find the value in the
+        /// queue with the reference, and sets
+        /// it to the new reference.
+        /// 
+        pub fn set_ref(&mut self, data: &'a F, new_ref: &'a F)
+            -> Result<(), Box<dyn std::error::Error>> {
+            let index = match self.map.get(data) {
+                Some(i) => *i,
+                None => return Err(Box::new(NotInQueue)),
+            };
+            let (old_val, _) = self.heap[index];
+            self.heap[index] = (old_val, new_ref);
+            self.map.remove(data);
+            self.map.insert(new_ref, index);
+            Ok(())
+        }
+
+        ///
+        /// Returns the number of elements
+        /// in the priority queue.
+        /// 
+        pub fn size(&self) -> usize {
+            self.heap.len()
         }
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use super::sorted_queue::*;
+
     #[test]
     fn compare_tuple() {
         let x = (4, "hello");
         let y = (4, "there");
         println!("{}", x > y);
+    }
+
+    #[derive(Hash, Eq, Debug)]
+    struct Employee<'a> {
+        name: &'a str,
+        id: u64,
+    }
+
+    impl<'a> PartialEq for Employee<'a> {
+        fn eq(&self, other: &Self) -> bool {
+            self.id == other.id
+        }
+    }
+
+    #[test]
+    fn simple_queue() {
+        let emp_list = vec![
+            Employee { name: "victor", id: 10237 },
+            Employee { name: "hugo", id: 2937 },
+            Employee { name: "marcus", id: 1902 },
+        ];
+        let mut queue = SortedQueue::new(true);
+        for i in 0..3 {
+            queue.enq(i, &emp_list[i]);
+        }
+        for i in 0..3 {
+            let (_, emp) = queue.deq().unwrap();
+            dbg!(emp);
+        }
     }
 }
